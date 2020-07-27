fb2578029c02c9c325a5ee6ab023da3fbed9db9c
LeeJunGyun <rainmaker0303@hanmail.net>
2020-07-27 18:14:10 +0900

    ConvertUtils 기능 추가. - convertObjToStringExcludeFields

--- spring-boot-common-example/src/main/java/org/example/common/utils/ConvertUtils.java
+++ spring-boot-common-example/src/main/java/org/example/common/utils/ConvertUtils.java
@@ -5,6 +5,7 @@ import org.springframework.util.ObjectUtils;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Set;
 
 @Slf4j
 public class ConvertUtils {
@@ -24,6 +25,34 @@ public class ConvertUtils {
                 field.setAccessible(true);
                 Object tmp = field.get(obj);
                 if ( !ObjectUtils.isEmpty(tmp)) {
+                    sb.append(field.get(obj));
+                    sb.append(separator);
+                }
+            } catch (IllegalAccessException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
+            }
+        });
+        return sb.length() == 0 ? "" : sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+    /**
+     * obj 안에 속성들의 값을 이어 붙인다. (특정 필드 제외)
+     * @param obj
+     * @param separator
+     * @param excludeFields
+     * @return
+     */
+    public static String convertObjToStringExcludeFields(
+            Object obj, String separator, Set<String> excludeFields) {
+        StringBuilder sb = new StringBuilder();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        Arrays.stream(fields).forEach(field -> {
+            try {
+                /** private 접근 가능 */
+                field.setAccessible(true);
+                Object tmp = field.get(obj);
+                if ( !ObjectUtils.isEmpty(tmp) && !excludeFields.contains(field.getName())) {
                     sb.append(field.get(obj));
                     sb.append(separator);
                 }
--- spring-boot-common-example/src/test/java/com/example/common/utils/ConvertUtilsTest.java
+++ spring-boot-common-example/src/test/java/com/example/common/utils/ConvertUtilsTest.java
@@ -4,6 +4,9 @@ import org.example.common.utils.ConvertUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class ConvertUtilsTest {
 
     @Test
@@ -20,6 +23,19 @@ public class ConvertUtilsTest {
         TestObject testObject = new TestObject();
         testObject.setB("bbb");
         String str = ConvertUtils.convertObjToString(testObject, ",");
+        Assert.assertEquals("bbb", str);
+    }
+
+    /**
+     * 특정 필드 제외 테스트
+     */
+    @Test
+    public void convertObjToStringExcludeFieldsTest() {
+        TestObject testObject = new TestObject();
+        testObject.setA("aaa");
+        testObject.setB("bbb");
+        String str = ConvertUtils.convertObjToStringExcludeFields(
+                testObject, ",", new HashSet<>(Arrays.asList("a")));
         Assert.assertEquals("bbb", str);
     }
 
